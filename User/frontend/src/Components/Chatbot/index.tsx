555397c8555aa2d0773e1da1d41e65e80d64a0c7
Mourad Rahi <dodirahi@gmail.com>
2023-04-14 14:54:25 +0300

    F fix: Chatbot

--- User/frontend/src/Components/Chatbot/index.tsx
+++ User/frontend/src/Components/Chatbot/index.tsx
@@ -1,62 +1,45 @@
-import { Card, TextInput, ScrollArea, Button, Flex, } from "@mantine/core";
-import { useCounter } from '@mantine/hooks';
-import { useState } from "react";
+import React, { useState } from "react";
+import { useMantineTheme } from "@mantine/core";
+import {  Button, TextInput } from "@mantine/core";
 
-const Chatbot = () => {
-    const keywords = ["hi","help","classes"];
-    const [submitted, setSubmitted] = useState(false);
-    const [question, setQuestion] = useState("");
-    const answer = "hello"
+const Chatbot: React.FC = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+  const theme = useMantineTheme();
 
-    const [count, handlers] = useCounter(3, { min: 0, max: 10 });
-    const content = Array(count)
-        .fill(0)
-        .map((_, index) => <p key={index}>{answer}</p>);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-    const asked = () => {
-        if (!question) {
-          setSubmitted(true);
-          
-        } else {
-          setSubmitted(false);
-          handlers.increment();
-        }
-      };
-    return(
-        <Flex align="center" justify="center" h="100vh">
-            
-            <Card style={{ background: "grey", minHeight: "250px" }} w="30%">
+  const handleInputSubmit = () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
 
-            <div style={{ height: "50px", width: "50px" }} />
-            <Flex direction="column" gap="md" h={160}>
-            <ScrollArea h={250} offsetScrollbars scrollbarSize={2} scrollHideDelay={0}>
-                {content}
-            </ScrollArea>
-                <TextInput
-                placeholder="Question"
-                error={submitted ? " " : ""}
-                radius="md"
-                onChange={(event) => {
-                    setQuestion(event.currentTarget.value);
-                    console.log(question);
-                }}
-                value={question}
-                />
-          </Flex>
-          <Flex align="center" justify="center" p="lg">
-            <Button
-              style={{ background: "#FFFF00" }}
-              radius="md"
-              size="md"
-              uppercase
-              onClick={asked}
-            >
-              send
-            </Button>
-          </Flex>
-        </Card>
-      </Flex>
-    );
+    setMessages([...messages, inputValue.trim()]);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <div>
+        <TextInput
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Type your message here..."
+          size="lg"
+          variant="filled"
+          style={{ marginBottom: theme.spacing.xs }}
+        />
+        <Button onClick={handleInputSubmit}>Send</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Chatbot;
