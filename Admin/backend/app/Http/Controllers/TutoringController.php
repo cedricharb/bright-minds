bd6c7f3ed22641f0faef9bb5dc96d9db80c98f20
larissa-abboud <lariabboud@gmail.com>
2023-04-16 22:41:25 +0300

    B: feat:tutoring controllers

--- Admin/backend/app/Http/Controllers/TutoringController.php
+++ Admin/backend/app/Http/Controllers/TutoringController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class TutoringController extends Controller
 {
-    //
+    public function viewTutoringSubjects() {
+     // list tutoring subjects
+       
+     }
+    public function addTutoringSubject() {
+     //add tutoring subject
+       
+     }
+    public function deleteTutoringSubject() {
+     //delete tutoring subject
+       
+     }
+    /*************sessions**************/
+    public function viewTutoringSessions() {
+     // list tutoring sessions
+       
+     }
+    public function addTutoringSession() {
+     //add tutoring Session
+       
+     }
+    public function deleteTutoringSession() {
+     //delete tutoring Session
+       
+     } 
+    public function setTutoringSession() {
+     //delete tutoring Session
+       
+     } 
 }
