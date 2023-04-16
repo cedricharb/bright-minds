476df2fddede3b52091cc7b5547452cb689aad98
larissa-abboud <lariabboud@gmail.com>
2023-04-16 21:08:23 +0300

    B: feat:classes-routes

--- Admin/backend/routes/api.php
+++ Admin/backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClassController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'classes'], function () {
+     /* Authentication Routes */
+     Route::get('/viewClasses', [ClassController::class,'viewClasses']);
+     Route::post('/addClass', [ClassController::class,'addClass']);
+     Route::post('/deleteClass', [ClassController::class,'deleteClass']);
+     Route::post('/editClass', [ClassController::class,'editClass']); //get id of class
+ 
+    });   
+    
+ 
+  });
