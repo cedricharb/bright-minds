40040fb61d250b86b3c9ca7d7e16821c0f60413a
larissa-abboud <lariabboud@gmail.com>
2023-04-16 22:34:57 +0300

    B: feat:classes-controllers

--- Admin/backend/app/Http/Controllers/ClassController.php
+++ Admin/backend/app/Http/Controllers/ClassController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class ClassController extends Controller
 {
-    //
+    public function viewClasses() {
+        //get all classes:Educlass
+        $classes=EduClass::all();
+        if ($classes->save()) {
+            return response()->json([
+                      'success' => true,
+                      'message' => 'Success',
+                      'data' => $classes
+                  ], 201);
+              } else {
+                  return response()->json([
+                      'success' => false,
+                      'message' => 'Fail',
+                  ], 400);
+          
+              }
+      }
+      public function addClass() {
+        //add class to db
+        
+      }
+      public function deleteClass() {
+         //delete class from db
+        
+      }
+      public function editClass() {
+         //edit class from db
+        
+      }
+
 }
