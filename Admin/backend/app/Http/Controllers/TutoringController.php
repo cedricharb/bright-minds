56305d7cab66b95d11bfd17368a21019b1b7f2cc
larissa-abboud <lariabboud@gmail.com>
2023-04-18 02:56:10 +0300

    B: feat:view/add/delete schedule

--- Admin/backend/app/Http/Controllers/TutoringController.php
+++ Admin/backend/app/Http/Controllers/TutoringController.php
@@ -75,8 +75,109 @@ class TutoringController extends Controller
             'message' => 'Success',
         ], 201);
     
-    }  
+    } 
+    /*************schedual**************/
+    } 
+   /* public  function add_tutoring_time($time,$request){
+        //take input, create entity ,add to array
+        $from= $request->input('from'); //take input
+        $to= $request->input('to'); //take input
+        $timings = array($from, $to);
+        return $timings;
+    }*/
+     public function addTutoringSchedule(Request $request) {
+     //add 
+     /**example
+      * Moday: { //day 
+		*	available: true;
+		*	timings: { //time :array
+		*		08:30-9:30:false; 
+		*		...
+		*		15:30-16:30:true;
+		*		17:30-18:30:true;
+		*		18:30-19:30:true;
+		*		...
+		*		20:30-21:30:false;
+		*	}
+		*}
+      */
+      
+      $request->validate([
+        'day'=>'required',
+        'available'=>'required',
+        //get timings? or ask for timings to be added
+
+    ]);
+    $schedule = new Schedule;
+    $schedule->day=$request->day;
+    $schedule->available=$request->available;
+
+    $check = Schedule::where('day', $request->day)->first();
+
+    if ($check) { //day is in our database\
+        return response()->json([
+            'success' => false,
+            'message' => 'error ,subject already exist',
+        ], 400);
+    }elseif ($schedule->save()) {
+        return response()->json([
+                  'success' => true,
+                  'message' => 'Success',
+                  'data' => $schedule
+              ], 201);
+          } else {
+              return response()->json([
+                  'success' => false,
+                  'message' => 'Fail',
+              ], 400);
+      
+          }    
      }
+    public function deleteTutoringSchedule(Request$request) {
+     //delete 
+     $schedule = Schedule::where('day', $request->day)->first();
+    
+     if (!$schedule) { //schedule isn't found in our database\
+         return response()->json([
+             'success' => false,
+             'message' => 'error ,schedule does not exist',
+         ], 400);
+     } else {
+     
+     $schedule->delete();
+     return response()->json([
+         'success' => true,
+         'message' => 'Success',
+     ], 201);
+ 
+ }  
+     } 
+    public function viewTutoringSchedule() {
+     //view 
+     $schedule = Schedule::get();
+        $subset = $schedule->map(function ($schedule) {
+            return collect($schedule->toArray())
+                ->only(['day','available'])
+                ->all();
+        });
+        if ($schedule->save()) {
+            return response()->json([
+                      'success' => true,
+                      'message' => 'Success',
+                      'data' => $subset
+                  ], 201);
+              } else {
+                  return response()->json([
+                      'success' => false,
+                      'message' => 'Fail',
+                  ], 400);
+          
+              }
+       
+     
+         
+     } 
+     
     /*************sessions**************/
     public function viewTutoringSessions() {
      // list tutoring sessions
@@ -93,17 +194,6 @@ class TutoringController extends Controller
     public function setTutoringSession() {
      //edit tutoring Session
        
-     } 
-     public function addTutoringSchedual() {
-     //add tutoring Session
-       
-     }
-    public function deleteTutoringSchedual() {
-     //delete tutoring Session
-       
-     } 
-    public function viewTutoringSchedual() {
-     //view tutoring Session
-       
-     } 
+      
+}
 }
