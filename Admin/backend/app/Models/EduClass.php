d6c309cb63e34929bc01965245e010043852c123
larissa-abboud <lariabboud@gmail.com>
2023-04-17 21:56:39 +0300

    B: fix:classes-db connection

--- Admin/backend/app/Models/EduClass.php
+++ Admin/backend/app/Models/EduClass.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class EduClass extends Model
 {
     use HasFactory;
+    protected $connection = 'mongodb';
 }
