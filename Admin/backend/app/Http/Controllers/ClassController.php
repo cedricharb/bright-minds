f36b8fbedb5bcc280ced109d5f3dfe19fd35bbe1
larissa-abboud <lariabboud@gmail.com>
2023-04-18 00:54:34 +0300

    B: feat:view/add class

--- Admin/backend/app/Http/Controllers/ClassController.php
+++ Admin/backend/app/Http/Controllers/ClassController.php
@@ -8,13 +8,18 @@ use App\Models\EduClass;
 class ClassController extends Controller
 {
     public function viewClasses() {
-        //get all classes:Educlass
-        $classes=EduClass::all();
+    //get all classes:Educlass
+        $classes = EduClass::get();
+        $subset = $classes->map(function ($classes) {
+            return collect($classes->toArray())
+                ->only(['title', 'description', 'age_range'])
+                ->all();
+        });
         if ($classes->save()) {
             return response()->json([
                       'success' => true,
                       'message' => 'Success',
-                      'data' => $classes
+                      'data' => $subset
                   ], 201);
               } else {
                   return response()->json([
@@ -24,12 +29,36 @@ class ClassController extends Controller
           
               }
       }
-      public function addClass() {
+      public function addClass(Request $request) {
         //add class to db
+        $request->validate([
+            'tile'=>'required',
+            'description'=>'required',
+            'age_range'=>'required', 
+        ]);
+        $class = new EduClass;
+        $class->title=$request->title;
+        $class->description=$request->description;
+        $class->age_range=$request->age_range;
         
+        if ($class->save()) {
+            return response()->json([
+                      'success' => true,
+                      'message' => 'Success',
+                      'data' => $class
+                  ], 201);
+              } else {
+                  return response()->json([
+                      'success' => false,
+                      'message' => 'Fail',
+                  ], 400);
+          
+              }
+            
       }
-      public function deleteClass() {
+      public function deleteClass(Request $request) {
          //delete class from db
+         
         
       }
       public function editClass() {
