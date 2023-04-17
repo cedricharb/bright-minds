062db61b10c1dff646c6087342b51103f592dcd1
larissa-abboud <lariabboud@gmail.com>
2023-04-18 01:39:07 +0300

    B: feat:view/add/delete subject

--- Admin/backend/app/Http/Controllers/TutoringController.php
+++ Admin/backend/app/Http/Controllers/TutoringController.php
@@ -8,15 +8,74 @@ class TutoringController extends Controller
 {
     public function viewTutoringSubjects() {
      // list tutoring subjects
+     $subject = Subject::get();
+        $subset = $subject->map(function ($subject) {
+            return collect($subject->toArray())
+                ->only(['subject_name'])
+                ->all();
+        });
+        if ($subject->save()) {
+            return response()->json([
+                      'success' => true,
+                      'message' => 'Success',
+                      'data' => $subset
+                  ], 201);
+              } else {
+                  return response()->json([
+                      'success' => false,
+                      'message' => 'Fail',
+                  ], 400);
+          
+              }
        
      }
-    public function addTutoringSubject() {
+    public function addTutoringSubject(Request $request) {
      //add tutoring subject
-       
+     $request->validate([
+        'subject_name'=>'required',
+    ]);
+    $subject = new Subject;
+    $subject->subject_name=$request->subject_name;
+    
+    $check = Subject::where('subject_name', $request->subject_name)->first();
+
+    if ($check) { //subject is in our database\
+        return response()->json([
+            'success' => false,
+            'message' => 'error ,subject already exist',
+        ], 400);
+    }elseif ($subject->save()) {
+        return response()->json([
+                  'success' => true,
+                  'message' => 'Success',
+                  'data' => $subject
+              ], 201);
+          } else {
+              return response()->json([
+                  'success' => false,
+                  'message' => 'Fail',
+              ], 400);
+      
+          }  
      }
-    public function deleteTutoringSubject() {
+    public function deleteTutoringSubject(Request $request) {
      //delete tutoring subject
-       
+     $subject = Subject::where('subject_name', $request->subject_name)->first();
+    
+        if (!$subject) { //subject isn't found in our database\
+            return response()->json([
+                'success' => false,
+                'message' => 'error ,subject does not exist',
+            ], 400);
+        } else {
+        
+        $subject->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Success',
+        ], 201);
+    
+    }  
      }
     /*************sessions**************/
     public function viewTutoringSessions() {
@@ -32,7 +91,19 @@ class TutoringController extends Controller
        
      } 
     public function setTutoringSession() {
+     //edit tutoring Session
+       
+     } 
+     public function addTutoringSchedual() {
+     //add tutoring Session
+       
+     }
+    public function deleteTutoringSchedual() {
      //delete tutoring Session
+       
+     } 
+    public function viewTutoringSchedual() {
+     //view tutoring Session
        
      } 
 }
