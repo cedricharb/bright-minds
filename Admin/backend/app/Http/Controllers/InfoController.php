18a918a816d681877819f27e624dfd0ec1387aeb
Hoda <hoda.hani.mawlawi@gmail.com>
2023-04-18 22:35:43 +0300

    B change typo in controller name

--- Admin/backend/app/Http/Controllers/InfoController.php
+++ Admin/backend/app/Http/Controllers/InfoController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\About;
 
-class InfoContoller extends Controller
+class InfoController extends Controller
 {
     //
 }
--- User/backend/app/Http/Controllers/InfoController.php
+++ User/backend/app/Http/Controllers/InfoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class InfoContoller extends Controller
+class InfoController extends Controller
 {
     //
 }
