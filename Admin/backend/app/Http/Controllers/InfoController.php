bb62b558c0b8d8a2b9b0d1dce1832b522559fc57
Hoda <hoda.hani.mawlawi@gmail.com>
2023-04-18 22:42:13 +0300

    B add getInfo for about page for admin

--- Admin/backend/app/Http/Controllers/InfoController.php
+++ Admin/backend/app/Http/Controllers/InfoController.php
@@ -7,5 +7,14 @@ use App\Models\About;
 
 class InfoController extends Controller
 {
-    //
+    public function getInfo() {
+        $about = About::find('643eeae3ed89046e426aa1bf'); //to-do: _id is an object idk if this will work
+        return response()->json([
+            "result" => true,
+            "general" => $about->general,
+            "mission" => $about->mission,
+            "vision" => $about->vision
+        ]);
+
+    }
 }
