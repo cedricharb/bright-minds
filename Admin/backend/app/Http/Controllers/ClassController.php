0af0083d6c3b51b2573f39f12754a0ba6e715921
larissa-abboud <lariabboud@gmail.com>
2023-04-18 03:48:30 +0300

    B: feat:FAQ/About/chatbot controllers

--- Admin/backend/app/Http/Controllers/ClassController.php
+++ Admin/backend/app/Http/Controllers/ClassController.php
@@ -7,4 +7,31 @@ use Illuminate\Http\Request;
 class ClassController extends Controller
 {
     //
+    public function viewFAQ() {
+     // list camps
+       
+     }
+    public function addFAQ() {
+     //add camp
+       
+     }
+    public function deleteFAQ() {
+     //delete camp
+    }
+
+    /********about****** */
+    public function viewAbout() {
+     // view about content
+       
+     }
+    public function addEdit() {
+     //edit general info
+       
+     }
+
+     /******chatbot ***** */
+     public function addChatbotFAQ() {
+     //record chatbot answer/question to be added to faq section
+       
+     }
 }
