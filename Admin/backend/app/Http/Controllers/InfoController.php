0ae1a0816080114d3129753c4142f5a3413c9de7
Hoda <hoda.hani.mawlawi@gmail.com>
2023-04-18 22:43:13 +0300

    B add updateInfo for about page for admin

--- Admin/backend/app/Http/Controllers/InfoController.php
+++ Admin/backend/app/Http/Controllers/InfoController.php
@@ -17,4 +17,36 @@ class InfoController extends Controller
         ]);
 
     }
+
+    public function updateInfo(Request $request) {
+        $about = About::find('643eeae3ed89046e426aa1bf'); //to-do: _id is an object idk if this will work
+        switch ($request) {
+            case 1:
+                $about->general = $request->editedText;
+                $about->save;
+                return response()->json([
+                    "result" => true
+                ]);
+                break;
+            case 2:
+                $about->mission = $request->editedText;
+                $about->save;
+                return response()->json([
+                    "result" => true
+                ]);
+                break;
+            case 3:
+                $about->vision = $request->editedText;
+                $about->save;
+                return response()->json([
+                    "result" => true
+                ]);
+                break;
+            default:
+                return response()->json([
+                    "result" => false
+                ]);
+                break;
+        }
+    }
 }
