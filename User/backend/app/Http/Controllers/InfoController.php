dfa7b3e6c2c0cbadd52f916bca76650bfddcb2cb
Hoda <hoda.hani.mawlawi@gmail.com>
2023-04-18 22:42:25 +0300

    B add getInfo for about page for user

--- User/backend/app/Http/Controllers/InfoController.php
+++ User/backend/app/Http/Controllers/InfoController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class InfoController extends Controller
 {
-    //
+    public function getInfo() {
+        $about = About::find('643eeae3ed89046e426aa1bf'); //to-do: _id is an object idk if this will work
+        return response()->json([
+            "result" => true,
+            "general" => $about->general,
+            "mission" => $about->mission,
+            "vision" => $about->vision
+        ]);
+
+    }
 }
