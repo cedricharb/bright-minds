18f2e998edcaa321d88b6b97dc751965ade29b93
Hoda <hoda.hani.mawlawi@gmail.com>
2023-04-19 17:22:02 +0300

    B fix spelling mistake

--- Admin/backend/routes/api.php
+++ Admin/backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InfoContoller;
+use App\Http\Controllers\InfoController;
 
 /*
 |--------------------------------------------------------------------------
