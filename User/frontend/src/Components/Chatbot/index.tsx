dcc7b8aa6e90e003b68091e3b1e97274d9329a49
Mourad Rahi <dodirahi@gmail.com>
2023-04-25 11:54:58 +0300

    F feat: chatbot skeleton

--- User/frontend/src/Components/Chatbot/index.tsx
+++ User/frontend/src/Components/Chatbot/index.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import { useMantineTheme } from "@mantine/core";
-import { Button, TextInput, Paper, StarIcon } from "@mantine/core";
+import { ActionIcon, useMantineTheme } from "@mantine/core";
+import { Button, TextInput, Paper } from "@mantine/core";
+import { IconStar } from "@tabler/icons-react";
+import { keywords } from '../../data/backendFodder'
 
-const Chatbot: React.FC = () => {
+
+const Chatbot = () => {
   const [messages, setMessages] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
   const [selectedStars, setSelectedStars] = useState<number>(0);
   const theme = useMantineTheme();
-  const keywords = ["hello", "hi", "hey", "bye", "goodbye"];
+
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -23,12 +26,13 @@ const Chatbot: React.FC = () => {
 
     // Check for keywords and respond accordingly0
     const lowerCaseInput = inputValue.toLowerCase();
+    
     for (const keyword of keywords) {
       if (lowerCaseInput.includes(keyword)) {
         if (keyword === "hello" || keyword === "hi" || keyword === "hey") {
           setMessages([
             ...messages,
-            "Hi there! How can I help you today?",
+            inputValue+"Hi there! How can I help you today?",
           ]);
         } else if (keyword === "bye" || keyword === "goodbye") {
           setMessages([
@@ -55,23 +59,11 @@ const Chatbot: React.FC = () => {
   };
 
   const renderStarIcon = (index: number) => {
-    if (index < selectedStars) {
       return (
-        <StarIcon
-          key={index}
-          onClick={() => handleStarClick(index)}
-          style={{ cursor: "pointer", color: theme.colors.yellow[7] }}
-          color="000000"
-        />
+        <ActionIcon key={index} color={index<selectedStars? 'yellow' : 'dark'} justify-content="flex-start" onClick={() => handleStarClick(index)}>
+          <IconStar />
+        </ActionIcon>
       );
-    }
-    return (
-      <StarIcon
-        key={index}
-        onClick={() => handleStarClick(index)}
-        style={{ cursor: "pointer" }}
-      />
-    );
   };
 
   return (
@@ -80,11 +72,10 @@ const Chatbot: React.FC = () => {
         {messages.map((message, index) => (
           <div key={index}>{message}</div>
         ))}
-        <div style={{ display: "flex", justifyContent: "flex-end" }}>
-          {[...Array(5)].map((_, index) => renderStarIcon(index))}
-        </div>
+        
       </Paper>
       <div style={{ display: "flex" }}>
+        
         <TextInput
           value={inputValue}
           onChange={handleInputChange}
@@ -99,6 +90,9 @@ const Chatbot: React.FC = () => {
           style={{ marginRight: theme.spacing.xs }}
         />
         <Button onClick={handleInputSubmit}>Send</Button>
+        <div style={{ display: "flex", justifyContent: "flex-start" }}>
+          {[...Array(5)].map((_, index) => renderStarIcon(index))}
+        </div>
       </div>
     </div>
   );
