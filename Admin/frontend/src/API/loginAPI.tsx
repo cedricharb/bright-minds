6829a5344cf74ba262228c8b6821ce2825cf00d4
Mourad Rahi <dodirahi@gmail.com>
2023-04-28 11:26:13 +0300

    F feat: login api added

---
+++ Admin/frontend/src/API/loginAPI.tsx
@@ -0,0 +1,21 @@
+const axios = require('axios');
+interface Response {
+	token: string;
+}
+function getQuotes() {
+	const options = {
+		method: 'POST',
+		url: 'http://127.0.0.1:8000/api/v1/admin/auth/login',
+		params: { email: '', password: '' },
+		headers: {
+		},
+	};
+	axios
+		.request(options)
+		.then(function ({ data }: { data: Response }) {
+			console.log(data);
+		})
+		.catch(function (error: any) {
+			console.error(error);
+		});
+}
