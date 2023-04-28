947df1b220618073d07c11c0e3d648d9fdbdeb8f
larissa-abboud <lariabboud@gmail.com>
2023-04-28 05:48:08 +0300

    B:fix:-routes-controllers

--- Admin/backend/app/Http/Controllers/ClassController.php
+++ Admin/backend/app/Http/Controllers/ClassController.php
@@ -120,35 +120,5 @@ class ClassController extends Controller
     }
       }
 
-    public function viewCamps() {
-     // list camps
-       
-     }
-    public function setCampTimings() {
-     // set camps timings
-       
-     }
-    public function addCamp() {
-     //add camp
-       
-     }
-    public function deleteCamp() {
-     //delete camp
-    }
-    public function editCampVisisbility() {
-    // edit camp visibility :: defeualt -> disabled ?? enabled
-
-    }
-    public function viewRegisteredCampers() {
-    // view Registered Campers :: list registered campers
-          
-    }
-    public function sendEmailToCampers() {
-    // send Email To campers :: get email of campers registered in specific camps
-              
-    }
-    public function viewCampReviews() {
-    // list Camp Reviews
-                  
-    }
+    
 }
