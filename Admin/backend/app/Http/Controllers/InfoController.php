52162e1a0380e9f24b989511b5e06f41c7710cde
larissa-abboud <lariabboud@gmail.com>
2023-04-29 21:28:41 +0300

    B:fix:auth password

--- Admin/backend/app/Http/Controllers/InfoController.php
+++ Admin/backend/app/Http/Controllers/InfoController.php
@@ -7,43 +7,5 @@ use App\Models\About;
 
 class InfoController extends Controller
 {
-    public function getInfo()
-    {
-        $about = About::find('643ef5c7ed89046e426aa1c2');
-        return response()->json([
-            "result" => true,
-            "general" => $about->general,
-            "mission" => $about->mission,
-            "vision" => $about->vision
-        ]);
-
-    }
-
-    public function updateInfo(Request $request)
-    {
-        $about = About::find('643ef5c7ed89046e426aa1c2');
-        $result;
-        switch ($request) {
-            case 1:
-                $about->general = $request->editedText;
-                $about->save;
-                $result = true;
-                break;
-            case 2:
-                $about->mission = $request->editedText;
-                $about->save;
-                $result = true;
-                break;
-            case 3:
-                $about->vision = $request->editedText;
-                $about->save;
-                $result = true;
-                break;
-            default:
-                $result = false;
-                break;
-        }
-        return response()->json([
-            "result" => $result
-        ]);
-    }
+    
+}
