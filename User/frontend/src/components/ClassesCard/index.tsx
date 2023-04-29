f85f91ea6e82631d0307ea712a3a800046d65a18
Sh4dowMonkey <cedric.elie.harb@gmail.com>
2023-04-29 12:07:38 +0300

    F feat: added classes card to the user website

---
+++ User/frontend/src/components/ClassesCard/index.tsx
@@ -0,0 +1,44 @@
+import { Button, Card, Flex, Text, useMantineTheme } from "@mantine/core";
+
+type Props = {
+  name: string;
+  description: string;
+};
+
+const ClassesCard = ({ name, description }: Props) => {
+  const theme = useMantineTheme();
+  return (
+    <Card
+      style={{
+        borderRadius: "30px",
+        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+      }}
+      bg={theme.colors.dark[7]}
+      p="xl"
+      w={350}
+    >
+      <Flex align="center" direction="column" gap="md">
+        <Text
+          weight={500}
+          size="lg"
+          style={{ marginBottom: "10px" }}
+          color={theme.colors.yellow[4]}
+        >
+          {name}
+        </Text>
+        <Text color={theme.colors.yellow[4]}>{description}</Text>
+        <Text
+          weight={500}
+          size="md"
+          style={{ marginTop: "10px" }}
+          color={theme.colors.yellow[4]}
+        >
+          Sample Exercice:
+        </Text>
+        <Button color="yellow">Click here to try an exercice!</Button>
+      </Flex>
+    </Card>
+  );
+};
+
+export default ClassesCard;
