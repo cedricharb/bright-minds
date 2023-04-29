635e65aac4dbf091b5cc5afe64a07e39553cb09f
Sh4dowMonkey <cedric.elie.harb@gmail.com>
2023-04-29 19:20:59 +0300

    F feat: user website FAQ page

---
+++ User/frontend/src/Components/Question/index.tsx
@@ -0,0 +1,34 @@
+import { Flex, Paper, Text, useMantineTheme } from "@mantine/core";
+
+type Props = {
+  question: string;
+  answer: string;
+};
+
+const Question = ({ question, answer }: Props) => {
+  const theme = useMantineTheme();
+
+  return (
+    <Paper bg={theme.colors.dark[7]} radius="lg" w="100%" p="lg">
+      <Flex direction="column">
+        {/* {!isQuestion && (
+          <UnstyledButton w="100%" h="100%">
+            <Text color={theme.colors.yellow[4]} size={30} align="center">
+              Add an FAQ +
+            </Text>
+          </UnstyledButton>
+        )} */}
+        <>
+          <Text color={theme.colors.yellow[4]} weight="bolder" w="100%">
+            {question}
+          </Text>
+          <Text color={theme.colors.yellow[4]} w="100%">
+            {answer}
+          </Text>
+        </>
+      </Flex>
+    </Paper>
+  );
+};
+
+export default Question;
--- User/frontend/src/pages/FAQ/index.tsx
+++ User/frontend/src/pages/FAQ/index.tsx
@@ -1,5 +1,28 @@
+import { Flex, Text, useMantineTheme } from "@mantine/core";
+import Question from "../../components/Question";
+import { questions } from "../../data/backendFodder";
+import BottomBar from "../../components/BottomBar";
+
 const FAQ = () => {
-  return;
+  const theme = useMantineTheme();
+
+  return (
+    <Flex bg={theme.colors.gray[4]} direction="column" justify="space-between">
+      <Flex w="100%" direction="column" gap="lg" mih="100vh">
+        <Flex w="100%">
+          <Text size={40} weight="bolder" w="100%" align="center">
+            Frequently Asked Questions
+          </Text>
+        </Flex>
+        <Flex direction="column" gap="md" p="xl">
+          {questions.map((question) => (
+            <Question question={question.question} answer={question.answer} />
+          ))}
+        </Flex>
+      </Flex>
+      <BottomBar />
+    </Flex>
+  );
 };
 
 export default FAQ;
