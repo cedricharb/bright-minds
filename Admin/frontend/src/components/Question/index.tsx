4a338b338ae1625cda93244f5d926c165344eb26
Sh4dowMonkey <cedric.elie.harb@gmail.com>
2023-04-29 19:54:04 +0300

    F frontend: new question modal

--- Admin/frontend/src/components/Question/index.tsx
+++ Admin/frontend/src/components/Question/index.tsx
@@ -1,10 +1,16 @@
 import {
+  Button,
+  Card,
   Flex,
+  Input,
+  Modal,
   Paper,
   Text,
   UnstyledButton,
   useMantineTheme,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 type Props = {
   isQuestion: boolean;
@@ -14,34 +20,71 @@ type Props = {
 
 const Question = ({ isQuestion, question, answer }: Props) => {
   const theme = useMantineTheme();
+  const [opened, { open, close }] = useDisclosure(false);
+  const [newQuestion, setNewQuestion] = useState<string>("");
+  const [newAnswer, setNewAnswer] = useState<string>("");
+  const [newKeywords, setNewKeywords] = useState<Array<string>>([]);
 
   return (
-    <Paper bg={theme.colors.dark[7]} radius="lg" w="100%" p="lg">
-      <Flex direction="column">
-        {!isQuestion && (
-          <UnstyledButton w="100%" h="100%">
-            <Text
-              color={theme.colors.yellow[4]}
-              size={30}
-              align="center"
-              weight="bold"
-            >
-              Add an FAQ +
+    <>
+      <Modal opened={opened} onClose={close} radius="30px" size="xl" centered>
+        <Card w="100%" h="100%" padding="md">
+          <Flex direction="column" gap="lg" align="center">
+            <Text w="100%" align="center">
+              Add a new question.
             </Text>
-          </UnstyledButton>
-        )}
-        {isQuestion && (
-          <>
-            <Text color={theme.colors.yellow[4]} weight="bolder" w="100%">
-              {question}
-            </Text>
-            <Text color={theme.colors.yellow[4]} w="100%">
-              {answer}
-            </Text>
-          </>
-        )}
-      </Flex>
-    </Paper>
+            <Input
+              w="100%"
+              placeholder="Question"
+              value={newQuestion}
+              onChange={(event) => setNewQuestion(event.target.value)}
+            />
+            <Input
+              w="100%"
+              placeholder="Answer"
+              value={newAnswer}
+              onChange={(event) => setNewAnswer(event.target.value)}
+            />
+            <Input
+              w="100%"
+              placeholder="Keywords (separated by ', ')"
+              onChange={(event) =>
+                setNewKeywords(event.target.value.split(", "))
+              }
+            />
+            <Button color="dark" w="200px">
+              Submit
+            </Button>
+          </Flex>
+        </Card>
+      </Modal>
+      <Paper bg={theme.colors.dark[7]} radius="lg" w="100%" p="lg">
+        <Flex direction="column">
+          {!isQuestion && (
+            <UnstyledButton w="100%" h="100%" onClick={open}>
+              <Text
+                color={theme.colors.yellow[4]}
+                size={30}
+                align="center"
+                weight="bold"
+              >
+                Add an FAQ +
+              </Text>
+            </UnstyledButton>
+          )}
+          {isQuestion && (
+            <>
+              <Text color={theme.colors.yellow[4]} weight="bolder" w="100%">
+                {question}
+              </Text>
+              <Text color={theme.colors.yellow[4]} w="100%">
+                {answer}
+              </Text>
+            </>
+          )}
+        </Flex>
+      </Paper>
+    </>
   );
 };
 
