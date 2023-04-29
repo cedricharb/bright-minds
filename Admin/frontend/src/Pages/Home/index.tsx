2213c29cea73b7b5bca673e8cefe9c7aadaf8b39
Sh4dowMonkey <cedric.elie.harb@gmail.com>
2023-04-29 10:48:11 +0300

    F chore: bottom bar is now a component on its own

--- Admin/frontend/src/Pages/Home/index.tsx
+++ Admin/frontend/src/Pages/Home/index.tsx
@@ -1,12 +1,7 @@
 import { Flex, Text, useMantineTheme } from "@mantine/core";
 import CircleComponent from "../../components/CircularComponent";
 import { mainDesc } from "../../data/backendFodder";
-import {
-  IconBrandFacebook,
-  IconBrandInstagram,
-  IconBrandYoutube,
-  IconBrandLinkedin,
-} from "@tabler/icons-react";
+import BottomBar from "../../components/BottomBar";
 
 const Home = () => {
   const theme = useMantineTheme();
@@ -72,38 +67,7 @@ const Home = () => {
           />
         </Flex>
       </Flex>
-      <Flex
-        bg={theme.colors.dark[7]}
-        h={250}
-        w="100%"
-        style={{ borderRadius: "30px 30px 0px 0px" }}
-        align="center"
-        justify="space-between"
-      >
-        <Flex direction="column" pl="lg">
-          <Text color={theme.colors.yellow[4]} size="lg">
-            Check out our frequently asked questions for more information.
-          </Text>
-          <Text color={theme.colors.yellow[4]} size="lg">
-            Email us at test@gmail.com for any further or more detailed
-            questions.
-          </Text>
-        </Flex>
-        <Flex gap="lg" p="lg">
-          <a href="https://www.facebook.com/">
-            <IconBrandFacebook color={theme.colors.yellow[4]} size={50} />
-          </a>
-          <a href="https://www.instagram.com/brightminds_center/">
-            <IconBrandInstagram color={theme.colors.yellow[4]} size={50} />
-          </a>
-          <a href="https://www.youtube.com/">
-            <IconBrandYoutube color={theme.colors.yellow[4]} size={50} />
-          </a>
-          <a href="https://www.linkedin.com/">
-            <IconBrandLinkedin color={theme.colors.yellow[4]} size={50} />
-          </a>
-        </Flex>
-      </Flex>
+      <BottomBar />
     </Flex>
   );
 };
