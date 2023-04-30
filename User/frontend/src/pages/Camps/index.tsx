475586425b265330864812abefb9a2605f0b4b9a
Sh4dowMonkey <cedric.elie.harb@gmail.com>
2023-04-30 15:50:21 +0300

    F fix: UI fixes

--- User/frontend/src/pages/Camps/index.tsx
+++ User/frontend/src/pages/Camps/index.tsx
@@ -2,6 +2,7 @@ import { Divider, Flex, Text, useMantineTheme } from "@mantine/core";
 import CampCard from "../../components/CampCard";
 import { camps } from "../../data/backendFodder";
 import { useState } from "react";
+import BottomBar from "../../components/BottomBar";
 
 const Camps = () => {
   const theme = useMantineTheme();
@@ -15,48 +16,58 @@ const Camps = () => {
   console.log(upcomingCamps);
 
   return (
-    <Flex direction="column" gap="lg" h="100%" bg={theme.colors.gray[4]} p="md">
-      <Flex direction="column">
-        <Text weight="bolder" size={40} align="center" p="md">
-          Take a look at our camps!
-        </Text>
+    <Flex direction="column" bg={theme.colors.gray[4]}>
+      <Flex
+        direction="column"
+        gap="lg"
+        h="100%"
+        bg={theme.colors.gray[4]}
+        p="md"
+        pb={100}
+      >
+        <Flex direction="column">
+          <Text weight="bolder" size={40} align="center" p="md">
+            Take a look at our camps!
+          </Text>
+        </Flex>
+        {upcomingCamps.length > 0 && (
+          <>
+            <Flex direction="column">
+              <Text weight="bold" size="xl" p="md">
+                Ongoing Camps
+              </Text>
+              <Divider my="md" color={theme.colors.dark[7]} />
+            </Flex>
+            <Flex gap="md" wrap="wrap">
+              {upcomingCamps.map((camp) => (
+                <CampCard
+                  title={camp.title}
+                  description={camp.description}
+                  isUpcoming
+                  startDate={camp.startDate}
+                  endDate={camp.endDate}
+                />
+              ))}
+            </Flex>
+          </>
+        )}
+        <Flex direction="column">
+          <Text weight="bold" size="xl" p="md">
+            Previous Camps
+          </Text>
+          <Divider my="md" color={theme.colors.dark[7]} />
+        </Flex>
+        <Flex gap="md" wrap="wrap" justify="space-around">
+          {previousCamps.map((camp) => (
+            <CampCard
+              title={camp.title}
+              description={camp.description}
+              endDate={camp.endDate}
+            />
+          ))}
+        </Flex>
       </Flex>
-      {upcomingCamps.length > 0 && (
-        <>
-          <Flex direction="column">
-            <Text weight="bold" size="xl" p="md">
-              Ongoing Camps
-            </Text>
-            <Divider my="md" color={theme.colors.dark[7]} />
-          </Flex>
-          <Flex gap="md" wrap="wrap">
-            {upcomingCamps.map((camp) => (
-              <CampCard
-                title={camp.title}
-                description={camp.description}
-                isUpcoming
-                startDate={camp.startDate}
-                endDate={camp.endDate}
-              />
-            ))}
-          </Flex>
-        </>
-      )}
-      <Flex direction="column">
-        <Text weight="bold" size="xl" p="md">
-          Previous Camps
-        </Text>
-        <Divider my="md" color={theme.colors.dark[7]} />
-      </Flex>
-      <Flex gap="md" wrap="wrap" justify="space-around">
-        {previousCamps.map((camp) => (
-          <CampCard
-            title={camp.title}
-            description={camp.description}
-            endDate={camp.endDate}
-          />
-        ))}
-      </Flex>
+      <BottomBar />
     </Flex>
   );
 };
