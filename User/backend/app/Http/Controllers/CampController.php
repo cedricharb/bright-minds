8203b52f35155e0f2d2fef00e4302011cff5819e
Hoda <hoda.hani.mawlawi@gmail.com>
2023-05-01 21:25:53 +0300

    B api to get upcoming camp and prev camp titles

--- User/backend/app/Http/Controllers/CampController.php
+++ User/backend/app/Http/Controllers/CampController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Camp;
+use Illuminate\Support\Carbon;
 
 class CampController extends Controller
 {
-    //
+    public function getCamps()
+    {
+        $upcoming_camp = Camp::latest()->firstOrFail();
+        $prev = Camp::latest()->skip(1);
+        $prev_camps = $prev->map(function ($prev) {
+            return collect($prev->toArray())
+                ->only(['title'])
+                ->all();
+        });
+
+        return response()->json([
+            "result" => true,
+            "message" => 'upcoming camp and previous camp titles',
+            "upcoming" => $upcoming_camp,
+            "prev" => $prev_camps
+        ], 200);
+    }
 }
