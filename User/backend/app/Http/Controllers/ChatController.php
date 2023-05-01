18062ffddd99155a3633c6c9375f5a6c39df1f85
Hoda <hoda.hani.mawlawi@gmail.com>
2023-05-01 20:47:37 +0300

    B add api when you open chat

--- User/backend/app/Http/Controllers/ChatController.php
+++ User/backend/app/Http/Controllers/ChatController.php
@@ -10,7 +10,32 @@ class ChatController extends Controller
 {
     public function openChat() {
         $faqs = FAQ::all();
-        return response()->json($faqs);
+        $faq_details = $faqs->map(function($faqs) {
+            return collect($faqs->toArray())
+                ->only(['id', 'answer', 'keywords'])
+                ->all();
+        });
+        return response()->json([
+            'result'=>true,
+            'message' => "faqs",
+            'data' => $faq_details
+        ], 200);
     }
-    
+    public function receiveFeedback(Request $request) {
+        $faq = FAQ::find($request->id);
+        $new_chat = new Chatbot;
+        $new_chat->answer = $faq->answer;
+        $new_chat->question = $request->question;
+        $new_chat->review = $request->review;
+        if ($new_chat->save()) {
+            return response()->json([
+                'result'=>true
+            ], 200);
+        } else {
+            return response()->json([
+                'result' => false, 
+                'message' => 'chat feedback not saved.'
+            ]);
+        }
+    }
 }
