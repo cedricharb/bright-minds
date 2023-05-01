0dff28349beb9cc9a4a0ab7e91cbf0a087d5e793
Hoda <hoda.hani.mawlawi@gmail.com>
2023-05-01 21:12:56 +0300

    B add api to get class titles

--- User/backend/app/Http/Controllers/ClassController.php
+++ User/backend/app/Http/Controllers/ClassController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EduClass;
 
 class ClassController extends Controller
 {
-    //
+    public function getClasses()
+    {
+        $classes = EduClass::get();
+        $class_titles = $classes->map(function ($classes) {
+            return collect($classes->toArray())
+                ->only(['title'])
+                ->all();
+        });
+        return response()->json([
+            'result'=>true,
+            'class_titles' => $class_titles
+        ], 200);
+    }
 }
