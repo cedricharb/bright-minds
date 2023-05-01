7e8f40f0db5346003059ae9e94469be039f79a62
Hoda <hoda.hani.mawlawi@gmail.com>
2023-05-01 11:23:01 +0300

    B fix api

--- User/backend/app/Http/Controllers/InfoController.php
+++ User/backend/app/Http/Controllers/InfoController.php
@@ -2,19 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\About;
 
 class InfoController extends Controller
 {
-    public function getInfo() 
+    public function getInfo()
     {
-        $about = About::find('643ef5c7ed89046e426aa1c2');
+        $about = About::latest()->firstOrFail();
         return response()->json([
             "result" => true,
             "general" => $about->general,
             "mission" => $about->mission,
             "vision" => $about->vision
         ]);
-
     }
+}
