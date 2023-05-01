269eeb630e42cf23d4c007752caf5206721e4f47
Hoda <hoda.hani.mawlawi@gmail.com>
2023-05-01 12:47:07 +0300

    B add openChat api

--- User/backend/app/Http/Controllers/CampController.php
+++ User/backend/app/Http/Controllers/CampController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\FAQ;
 
 class CampController extends Controller
 {
-    //
+    public function openChat() {
+        $faqs = FAQ::all();
+        return response()->json($faqs);
+    }
 }
